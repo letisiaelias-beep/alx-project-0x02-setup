0b0ae21423bb8b98b3423096e07dc93c34bf3036
letisiaelias-beep <letisiaelias@gmail.com>
2025-09-25 23:29:47 +0300

    Update index.ts

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -7,3 +7,9 @@ export interface CardProps {
   content: string;
 }
 
+export interface ButtonProps {
+  label: string;
+  size: "small" | "medium" | "large";
+  shape: "rounded-sm" | "rounded-md" | "rounded-full";
+  onClick?: () => void;
+}
