ec397029e17ea6b71d1ad9a140e5bde18c1a89d8
letisiaelias-beep <letisiaelias@gmail.com>
2025-09-25 23:25:56 +0300

    Update about.tsx

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -17,3 +17,10 @@ export default function AboutPage() {
     </div>
   );
 }
+
+export interface ButtonProps {
+  label: string;
+  size: "small" | "medium" | "large";
+  shape: "rounded-sm" | "rounded-md" | "rounded-full";
+  onClick?: () => void;
+}
