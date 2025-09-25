d761107abc13300e7775d95797295d1393e01bff
letisiaelias-beep <letisiaelias@gmail.com>
2025-09-25 21:25:29 +0300

    Update Card.tsx

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { CardProps } from "../../interfaces";
+import { type CardProps } from "@/interfaces";
 
 export default function Card({ title, content }: CardProps) {
   return (
